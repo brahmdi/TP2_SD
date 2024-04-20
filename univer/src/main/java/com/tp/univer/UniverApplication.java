4010ceaa63c6d4df3f6ff7f964cd9749b893c1f9
brahmdi <96790699+brahmdi@users.noreply.github.com>
2024-04-20 23:57:56 +0100

    Add files via upload

---
+++ univer/src/main/java/com/tp/univer/UniverApplication.java
@@ -0,0 +1,55 @@
+package com.tp.univer;
+
+import com.tp.univer.entities.Product;
+import com.tp.univer.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class UniverApplication implements CommandLineRunner {
+	@Autowired
+	private ProductRepository productRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(UniverApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		productRepository.save(new Product(null,"Computer",4300,3));
+//		productRepository.save(new Product(null,"Printer",1200,4));
+//		productRepository.save(new Product(null,"Smart Phone",3200,32));
+//		List<Product> products = productRepository.findAll();
+//		products.forEach(p -> {
+//			System.out.println(p.toString());
+//		});
+//		Product product =productRepository.findById(Long.valueOf(3)).get();
+//		System.out.println("*******************");
+//		System.out.println(product.getId());
+//		System.out.println(product.getName());
+//		System.out.println(product.getQuantity());
+//		System.out.println("*******************");
+//		System.out.println("Mettre à jour un produit :");
+//        Product produit = productRepository.findById(Long.valueOf(1)).orElse(null);
+//        if (produit != null) {
+//            produit.setName("SMARTPHONE");
+//            productRepository.save(produit);
+//        } else {
+//            System.out.println("Product not found");
+//        }
+//		System.out.println("Supprimer un produit :");
+//        productRepository.deleteById(Long.valueOf(2));
+		List <Product> productList =productRepository.findByNameContains("S");
+		productList.forEach(p->{
+			System.out.println(p);
+		});
+		System.out.println("------------------------------");
+		List <Product> productList2 =productRepository.search("%S%");
+		productList2.forEach(p->{
+			System.out.println(p);
+		});
+	}
+}
